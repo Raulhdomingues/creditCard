d1493e5e959060eb5aba56556d386e8c6559b7f3
Raul Domingues <99521069+Raulhdomingues@users.noreply.github.com>
2023-11-14 18:20:23 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+const alpine = require('@alpinejs/mask');
+
+const nome = document.querySelector('#name');
+const numero = document.querySelector('#number');
+const mes = document.querySelector('.mes');
+const ano = document.querySelector('.ano');
+const cvc = document.querySelector('#cvc');
+
+const numeroCartao = document.querySelector('.card-number');
+const nomeCartao = document.querySelector('.card-name');
+const expiracaoMes = document.querySelector('#exp-mes');
+const expiracaoAno = document.querySelector('#exp-ano');
+
+const codigoSeguranca = document.querySelector('.tarja-cvc');
+
+
+numero.addEventListener('input', () => {
+    numeroCartao.textContent = numero.value;
+})
+
+nome.addEventListener('input', () => {
+    nomeCartao.textContent = nome.value;
+});
+
+mes.addEventListener('input', () => {
+    expiracaoMes.textContent = mes.value;
+})
+
+
+ano.addEventListener('input', () => {
+    expiracaoAno.textContent = ano.value;
+})
+
+cvc.addEventListener('input', () => {
+    codigoSeguranca.textContent = cvc.value;
+})
+
